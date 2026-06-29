f8912f1c408d2f1493734b3afdfacd8c6122ca0b
whitebian21 <danwhitney16@gmail.com>
2026-06-29 11:16:21 +0100

    first commit

---
+++ CLASS2/JAVASCRIPT/task.js
@@ -0,0 +1,19 @@
+// Function Declaration
+
+function school() {
+    console.log("MRsoft is the name of my school")
+};
+school();
+
+// Function Expression
+
+let girl = function() {
+    console.log("She is tall")
+};
+girl();
+
+// Arrow Function
+let land = () => {
+    console.log("The land is expensive")
+};
+land();
